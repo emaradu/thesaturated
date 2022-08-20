f3ed5e58ef9b4378aedbd83d6f82394fd4964496
Ema Radu <ema.radu05@icloud.com>
2022-08-20 14:09:22 +0300

    mesaj

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
-import './App.css';
+import "./App.css";
+import { Header } from "./components/Header/Header";
 
 function App() {
   return (
-    <div>
-      <p>ema radu aici</p>
-      <p>ema radu aici</p>
-      <p>ema radu aici</p>
+    <div className="main">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import "./header.css";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo"> the saturated </div>
+      <div className="middle">
+        <div> Category </div>
+        <div> Category 2 </div>
+        <div> Category 3 </div>
+      </div>
+    </div>
+  );
+};
